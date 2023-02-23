267c10bacd808e0c36696eaad658adf177cf1f4c
Luciano Tito 1801 <lucianotitocedron@gmail.com>
2023-02-22 23:53:36 -0300

    last detail

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import Header from '../components/Pokedex/Shared/Header'
 import { setNameTrainer } from '../store/slices/trainerName.slice'
 import '../styles/home.css'
 
